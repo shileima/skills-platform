5762d1565290c0c3d23ff02122f2f0d04db91483
mashilei <mashilei@meituan.com>
2026-08-05 23:52:35 +0800

    feat(TEST): add skills array for skill management
    - Introduced a new `TEST.ts` file containing an array of skill identifiers to facilitate skill management and integration.

---
+++ TEST.ts
@@ -0,0 +1,9 @@
+export const skills =[
+        "cskill-16971ce8-d",
+        "cskill-b8573d90-2",
+        "cskill-2e84a7a8-3", 
+        "cskill-4ec598f8-e",
+        "cskill-f77664f5-1",
+        "cskill-e5bab31c-3",
+        "cskill-54167986-f"
+    ]
